fd1df81cf40a0620780225fed417c623d69cf75b
mena5800 <mr.safwat55@gmail.com>
2024-01-01 08:56:38 +0200

    update database script

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,11 +4,11 @@
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
 -- create new user
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY "hbnb_test_pwd";
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY "hbnb_test_pwd";
 
 -- give user all privileges
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 
 -- give user select privilege on the database performance_schema 
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
